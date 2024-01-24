e7156a956618c91f1c973679ecd1a2cabef41365
royal0817 <martinezl27737@gmail.com>
2024-01-24 15:45:50 -0500

    first progress on menu

--- src/components/CakeMenu.jsx
+++ src/components/CakeMenu.jsx
@@ -1,13 +1,54 @@
 import React from 'react';
+import '../styles/CakeMenu.css'
+import img2  from '../images/logo.png'
 
 const CakeMenu = () => {
   return (
-    <div>
+    <div id='cake-menu'>
+      <div className='menu-logo'>
+        <img src={img2} alt="logo" />
+      </div>
+      <section className='menu-headers'>
       <h1>Menu</h1>
-      <h2>Cakes</h2>
-      <h2>Cupcakes</h2>
-      <h2>Cookkies</h2>
-      <h2>Food</h2>
+      </section>
+
+      <section className='menu-items'>
+        <h4>Cakes</h4>
+          <div>Tres Leches</div>
+          <div>Chocolate</div>
+          <div>Vanilla</div>
+          <div>Strawberry</div>
+          <div>flavor</div>
+          <div>flavor</div>
+          <div>flavor</div>
+          <div>flavor</div>
+
+        <h4>Cupcakes</h4>
+          <div>flavor</div>
+          <div>flavor</div>
+          <div>flavor</div>
+          <div>flavor</div>
+          <div>flavor</div>
+
+          <h4>Cookies</h4>
+            <div>flavor</div>
+            <div>flavor</div>
+            <div>flavor</div>
+            <div>flavor</div>
+            <div>flavor</div>
+            <div>flavor</div>
+            <div>flavor</div>
+        
+        <h4>Food</h4>
+          <div>food item</div>
+          <div>food item</div>
+          <div>food item</div>
+          <div>food item</div>
+          <div>food item</div>
+          <div>food item</div>
+          <div>food item</div>
+
+      </section>
     </div>
   );
 }
