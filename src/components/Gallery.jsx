e186ebe703e3bddb90ef116a6f1dbbea8996229d
royal0817 <martinezl27737@gmail.com>
2024-01-25 10:12:56 -0500

    updated carousel

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import Carousel from './carousel.jsx';
 import '../styles/carousel.css'
+import img1 from '../images/gallery/IMG_0321.jpeg'
+import img2 from '../images/gallery/IMG_0321.jpeg'
+import img3 from '../images/gallery/IMG_0999.jpeg'
+import img4 from '../images/gallery/IMG_8315.jpeg'
 
 const images = [
-  'https://placekitten.com/800/400',
-  'https://placekitten.com/801/400',
-  'https://placekitten.com/802/400',
-  'https://placekitten.com/803/400',
-  'https://placekitten.com/800/400',
-  'https://placekitten.com/801/400',
-  'https://placekitten.com/802/400',
-  'https://placekitten.com/803/400',
+  img1,img2,img3,img4
 ];
 
 const Gallery = () => {
   return (
   <div id='gallery'>
+    <h1>Gallery</h1>
     <Carousel images={images}/>
   </div>
   );
--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,36 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import PropTypes from 'prop-types';
+//-------------------------------!!!!!!!indicators not rendering come back to fix!!!!!!!!!!---------------------
 
-const Carousel = ({ images, interval = 2500 }) => {
+const CarouselIndicators = ({ images, activeIndex, onClick }) => (
+  <div className="carousel_indicators">
+    {images.map((_, index) => (
+      <span
+        key={index}
+        className={`carousel_indicator ${index === activeIndex ? 'active' : ''}`}
+        onClick={() => onClick(index)}
+      />
+    ))}
+  </div>
+);
+
+CarouselIndicators.propTypes = {
+  images: PropTypes.array.isRequired,
+  activeIndex: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+const Carousel = ({ images, interval = 3000 }) => {
   const [activeIndex, setActiveIndex] = useState(0);
-  const nextSlide = () => {
-    setActiveIndex((prevIndex) =>
-    prevIndex === images.length - 1 ? 0 : prevIndex + 1
-    );
-  };
+
+  const nextSlide = useCallback(() => {
+    setActiveIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  }, [images.length]);
+
+  const prevSlide = useCallback(() => {
+    setActiveIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  }, [images.length]);
+
+  const goToSlide = useCallback((index) => {
+    setActiveIndex(index);
+  }, []);
+
   useEffect(() => {
-    const nextSlide = () => {
-      setActiveIndex((prevIndex) =>
-        prevIndex === images.length - 1 ? 0 : prevIndex + 1
-      );
-    };
-  
     const autoPlayInterval = setInterval(nextSlide, interval);
-  
+
     return () => {
       clearInterval(autoPlayInterval);
     };
-  }, [interval, images.length]);
-  
-  
-  
-  const prevSlide = () => {
-    setActiveIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    );
-  };
+  }, [interval, nextSlide]);
+
   return (
     <div className="carousel">
-      <button onClick={prevSlide} className="carousel__btn carousel__btn--prev">
+      <button onClick={prevSlide} className="carousel__btn carousel__btn--prev" aria-label="Previous Slide">
         &lt;
       </button>
       <img
@@ -38,10 +53,21 @@ const Carousel = ({ images, interval = 2500 }) => {
         alt={`Slide ${activeIndex}`}
         className="carousel__img"
       />
-      <button onClick={nextSlide} className="carousel__btn carousel__btn--next">
+      <button onClick={nextSlide} className="carousel__btn carousel__btn--next" aria-label="Next Slide">
         &gt;
       </button>
+      <CarouselIndicators
+        images={images}
+        activeIndex={activeIndex}
+        onClick={goToSlide}
+      />
     </div>
   );
 };
+
+Carousel.propTypes = {
+  images: PropTypes.array.isRequired,
+  interval: PropTypes.number,
+};
+
 export default Carousel;
