36abd7c9074ba47cd6e6b7e35a8107e8e47b7ad9
royal0817 <martinezl27737@gmail.com>
2024-01-25 10:27:37 -0500

    adjusted menu

--- src/components/CakeMenu.jsx
+++ src/components/CakeMenu.jsx
@@ -8,46 +8,36 @@ const CakeMenu = () => {
       <div className='menu-logo'>
         <img src={img2} alt="logo" />
       </div>
-      <section >
-        <h1>Menu</h1>
-      </section>
 
       <section className='menu-items'>
+        <h1>Menu</h1>
         <h4>Cakes</h4>
           <div className='food-item'>Tres Leches</div>
+          <div className='food-item'>Cafe & Tres Leches</div>
           <div className='food-item'>Chocolate</div>
           <div className='food-item'>Vanilla</div>
           <div className='food-item'>Strawberry</div>
-          <div className='food-item'>flavor</div>
-          <div className='food-item'>flavor</div>
-          <div className='food-item'>flavor</div>
-          <div className='food-item'>flavor</div>
+          <div className='food-item'>Carrot Cake</div>
+          <div className='food-item'>Coconut</div>
+          <div className='food-item'>Lemon</div>
 
         <h4>Cupcakes</h4>
-          <div className='food-item'>flavor</div>
-          <div className='food-item'>flavor</div>
-          <div className='food-item'>flavor</div>
-          <div className='food-item'>flavor</div>
-          <div className='food-item'>flavor</div>
+          <div className='food-item'>Strawberry</div>
+          <div className='food-item'>Red Velvet</div>
+          <div className='food-item'>Chocolate</div>
+          <div className='food-item'>Vanilla</div>
+          <div className='food-item'>Lemon</div>
+          <div className='food-item'>Oreo</div>
+          <div className='food-item'>Tres Leches</div>
+          <div className='food-item'>Cafe & Tres Leches</div>
+
 
           <h4>Cookies</h4>
-            <div className='food-item'>flavor</div>
-            <div className='food-item'>flavor</div>
-            <div className='food-item'>flavor</div>
-            <div className='food-item'>flavor</div>
-            <div className='food-item'>flavor</div>
-            <div className='food-item'>flavor</div>
-            <div className='food-item'>flavor</div>
-        
-        <h4>Food</h4>
-          <div className='food-item'>food item</div>
-          <div className='food-item'>food item</div>
-          <div className='food-item'>food item</div>
-          <div className='food-item'>food item</div>
-          <div className='food-item'>food item</div>
-          <div className='food-item'>food item</div>
-          <div className='food-item'>food item</div>
-
+            <div className='food-item'>Chocolate Chip</div>
+            <div className='food-item'>Snickerdoodle</div>
+            <div className='food-item'>Oatmeal Raisin</div>
+            <div className='food-item'>Sugar Cookie</div>
+      
       </section>
     </div>
   );
